60b00c6fe5789321094fa97cbfb462d418fad567
ashwin.krishna <ashwin.krishna@razorthink.net>
2015-06-03 11:12:31 +0530

    audio call to test

--- public/js/nwk-videochat.directive.js
+++ public/js/nwk-videochat.directive.js
@@ -117,7 +117,7 @@ angular.module('nwk-videochat')
                     function initSelfVideo(cb) {
                         navigator.getUserMedia({
                             audio: true, // constraints for the call
-                            video: true
+                            video: false
                         }, function(stream) {
                             $scope.localStream = stream; // sucess call back
                             $scope.localVdoURL = $sce.trustAsResourceUrl(URL.createObjectURL(stream));
